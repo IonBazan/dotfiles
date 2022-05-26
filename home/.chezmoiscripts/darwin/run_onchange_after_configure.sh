acbf9da5203228cd436075a3f25bb2b17d2dde9f
Ion Bazan <ion.bazan@gmail.com>
2022-05-26 17:53:49 +0800

    update locales

--- home/.chezmoiscripts/darwin/run_onchange_after_configure.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure.sh
@@ -7,10 +7,22 @@ fi
 set -eufo pipefail
 
 defaults write NSGlobalDomain AppleInterfaceStyle Dark
-defaults write NSGlobalDomain AppleLanguages -array en-US
-defaults write NSGlobalDomain AppleLocale pl_PL
+defaults write NSGlobalDomain AppleLanguages -array en-SG
+defaults write NSGlobalDomain AppleLocale en_SG
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write NSGlobalDomain KeyRepeat -int 2
+
+# Enable `Tap to click`
+defaults write com.apple.AppleMultitouchTrackpad.plist Clicking -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Map `click or tap with two fingers` to the secondary click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 0
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 0
 
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
